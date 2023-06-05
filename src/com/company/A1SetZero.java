82de2efed00c6e7be5f46483df8d674ad95857d5
Vikas Bishnoi <vikasbishnoi9928@gmail.com>
2023-06-05 19:45:55 +0530

    problem 1 solved

---
+++ src/com/company/A1SetZero.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class A1SetZero {
+    public static void main(String[] args) {
+
+    }
+
+}
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        boolean[] row=new boolean[n];
+        boolean[] col=new boolean[m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    row[i]=true;
+                    col[j]=true;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i] || col[j]){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
